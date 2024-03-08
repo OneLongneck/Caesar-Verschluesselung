8248f5f3f4eab03820e61bff8acb678c1c58d203
OneLongneck <dylanhupfauf@gmail.com>
2024-03-08 21:04:32 +0100

    Fehlerbehebung und Implementierung der Entschlüsselung

--- caesar.py
+++ caesar.py
@@ -5,15 +5,25 @@ def Verschlüsseln():
     inputbuchstaben = [*Eingabe]  # wandelt den eingegebenen String in eine Liste der einzelnen Buchstaben um
 
     for x in inputbuchstaben:
-        zahlen.append(ord(x) + Rotzahl)        # weist jedem Buchstaben der Liste die dazugehörige Zahl anhand der Reihenfolge im Alphabet zu
-
+        zahlen.append((ord(x) - 96 + Rotzahl) % 26)        # weist jedem Buchstaben der Liste die dazugehörige Zahl anhand der Reihenfolge im Alphabet zu
     for y in zahlen:
-        text.append(chr(y))
+        text.append(chr(y + 96))
 
     Ausgabe = "".join(text)
-    print("Verschlüsselt ergibt dein Tex:\n" + Ausgabe)
+    print("Verschlüsselt ergibt dein Text:\n" + Ausgabe)
 
-Auswahl = input("Gib 1 ein für Verschlüsseln, 2 für Entschlüsseln\n")
+def Entschlüsseln():
+    inputbuchstaben = [*Eingabe]
+
+    for x in inputbuchstaben:
+        zahlen.append((ord(x) - 96 + Rotzahl) % 26)
+    for y in zahlen:
+        text.append(chr(y + 96))
+
+    Ausgabe = "".join(text)
+    print("Entschlüsselt ergibt dein Text:\n" + Ausgabe)
+
+Auswahl = input("Gib für Verschlüsseln 1 ein, für Entschlüsseln 2\n")
 Eingabe = input("Was möchtest du Ver-/Entschlüsseln?\n")
 Rotzahl = int(input("Was ist die Rotationszahl?\n"))
 
