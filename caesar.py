c7ec1b3c1692777f0bd1ca87048f0e80803d82b4
OneLongneck <dylanhupfauf@gmail.com>
2024-03-09 21:55:43 +0100

    Bugfix und Aufräumen des Codes

--- caesar.py
+++ caesar.py
@@ -2,23 +2,12 @@ zahlen = []
 text = []
 
 def Verschlüsseln():
-    inputbuchstaben = [*Eingabe]  # wandelt den eingegebenen String in eine Liste der einzelnen Buchstaben um
-
     for x in inputbuchstaben:
         zahlen.append((ord(x) - 96 + Rotzahl) % 26)        # weist jedem Buchstaben der Liste die dazugehörige Zahl anhand der Reihenfolge im Alphabet zu
-    for y in zahlen:
-        text.append(chr(y + 96))
-
-    Ausgabe = "".join(text)
-    print("Verschlüsselt ergibt dein Text:\n" + Ausgabe)
 
 def Entschlüsseln():
-    inputbuchstaben = [*Eingabe]
-
     for x in inputbuchstaben:
-        zahlen.append((ord(x) - 96 + Rotzahl) % 26)
-    for y in zahlen:
-        text.append(chr(y + 96))
+        zahlen.append((ord(x) - 96 - Rotzahl) % 26)
 
     Ausgabe = "".join(text)
     print("Entschlüsselt ergibt dein Text:\n" + Ausgabe)
@@ -27,7 +16,15 @@ Auswahl = input("Gib für Verschlüsseln 1 ein, für Entschlüsseln 2\n")
 Eingabe = input("Was möchtest du Ver-/Entschlüsseln?\n")
 Rotzahl = int(input("Was ist die Rotationszahl?\n"))
 
+inputbuchstaben = [*Eingabe]  # wandelt den eingegebenen String in eine Liste der einzelnen Buchstaben um
+
 if Auswahl == "1":
     Verschlüsseln()
 elif Auswahl == "2":
     Entschlüsseln()
+
+for y in zahlen:
+        text.append(chr(y + 96))
+
+Ausgabe = "".join(text)
+print("Dein neuer Text ist:\n" + Ausgabe)
